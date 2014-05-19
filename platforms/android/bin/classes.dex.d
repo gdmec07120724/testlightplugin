27d7e3a00db447fad6fe0d609f368c9227e27f32
gdmec07120724 <1196288905@qq.com>
2014-05-19 11:13:32 +0800

    testlightplugin

---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,11 @@
+C:\testlightplugin\platforms\android\bin\classes.dex : \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\t00385\lightsensor\LightSensorListener$1.class \
+C:\testlightplugin\platforms\android\bin\classes\cn\edu\gdmec\t00385\lightsensor\LightSensorListener.class \
+C:\testlightplugin\platforms\android\bin\classes\com\phonegap\helloworld\BuildConfig.class \
+C:\testlightplugin\platforms\android\bin\classes\com\phonegap\helloworld\HelloWorld.class \
+C:\testlightplugin\platforms\android\bin\classes\com\phonegap\helloworld\R$attr.class \
+C:\testlightplugin\platforms\android\bin\classes\com\phonegap\helloworld\R$drawable.class \
+C:\testlightplugin\platforms\android\bin\classes\com\phonegap\helloworld\R$string.class \
+C:\testlightplugin\platforms\android\bin\classes\com\phonegap\helloworld\R$xml.class \
+C:\testlightplugin\platforms\android\bin\classes\com\phonegap\helloworld\R.class \
+C:\testlightplugin\platforms\android\bin\dexedLibs\classes-fc16f74325d8233d7e8d00ca88dba06b.jar \
